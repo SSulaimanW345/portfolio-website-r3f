3ef2c22046165de4a3fb8e6bc90a4ce3b61bbbeb
SSulaimanW345 <s.s.waleed.e60@gmail.com>
2025-02-05 21:26:59 +0400

    code cleanup 5-2-25

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
-// import Hero from './sections/Hero.jsx';
-// import About from './sections/About.jsx';
 import { MainLayout } from './components/layout/MainLayout.jsx';
-// import Contact from './sections/Contact.jsx';
-// import Clients from './sections/Clients.jsx';
-// import Projects from './sections/Projects.jsx';
-// import WorkExperience from './sections/Experience.jsx';
 import { ProjectPage } from './pages/ProjectsPage.jsx';
 import { HomePage } from './pages/HomePage.jsx';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
@@ -19,22 +13,6 @@ const App = () => {
         </Routes>
       </MainLayout>
     </Router>
-    // <main className="w-full mx-auto relative">
-    //   <Navbar />
-    //   <Hero />
-    //   <WorkExperience />
-    //   <About />
-    //   <Projects />
-    //   <Clients />
-    //   <Contact />
-    //   <Footer />
-    //   {/* <About />
-    //   <Projects />
-    //   <Clients />
-    //   <WorkExperience />
-    //   <Contact />
-    //   <Footer /> */}
-    // </main>
   );
 };
 
--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -4,15 +4,6 @@ import { Background } from './Background.jsx';
 import { HeroContent } from './HeroContent.jsx';
 import { ResumeDownloadButton } from '../../ui/buttons/DownloadBtn.jsx';
 export const Hero = () => {
-  // const x = useControls({
-  //   rotationX: { value: 0, min: Math.PI * -2, max: Math.PI * 2, step: 0.01 },
-  //   rotationY: { value: 0, min: Math.PI * -2, max: Math.PI * 2, step: 0.01 },
-  //   rotationZ: { value: 0, min: Math.PI * -2, max: Math.PI * 2, step: 0.01 },
-  //   scale: { value: 7, min: 0.01, max: 20, step: 0.1 },
-  //   positionX: { value: 0, min: -15, max: 5, step: 0.1 },
-  //   positionY: { value: 0, min: -5, max: 5, step: 0.1 },
-  //   positionZ: { value: 0, min: -5, max: 5, step: 0.1 },
-  // });
   return (
     <section className="min-h-screen w-full flex relative" id="home">
       <Background />
@@ -22,9 +13,9 @@ export const Hero = () => {
         </p>
         <p className="hero_tag text-gray_gradient lg:hidden">Building Products & Brands</p>
       </div>
-      {/* <div className="absolute -translate-x-1/2 top-1/2 left-1/2 -translate-y-1/2 w-1/2 h-1/2  intro-animation">
+      <div className="absolute -translate-x-1/2 top-1/2 left-1/2 -translate-y-1/2 w-1/2 h-1/2  intro-animation">
         <HeroCanvas />
-      </div> */}
+      </div>
       <div className="absolute -translate-x-1/2 bottom-2 left-1/2 -translate-y-1/2">
         <HeroButtons />
       </div>
