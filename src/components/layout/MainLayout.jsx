aee85aadb60bc25dd94ffe1686a330c9614e5218
SSulaimanW345 <s.s.waleed.e60@gmail.com>
2025-02-05 23:18:48 +0400

    added page loader

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,10 +1,31 @@
 import Footer from './footer/Footer';
 import { Navbar } from './navbar/Navbar';
+import React, { useState, useEffect } from 'react';
+import { SiteLoader } from '../ui/other/SiteLoader';
+export const MainLayout = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
 
-export const MainLayout = ({ children }) => (
-  <main className="w-full mx-auto relative">
-    <Navbar />
-    {children}
-    <Footer />
-  </main>
-);
+  useEffect(() => {
+    const handleLoad = () => {
+      setTimeout(() => setIsLoading(false), 500); // Small delay for smooth transition
+    };
+
+    if (document.readyState === 'complete') {
+      handleLoad(); // If already loaded
+    } else {
+      window.addEventListener('load', handleLoad); // Wait for full page load
+    }
+
+    return () => window.removeEventListener('load', handleLoad);
+  }, []);
+  return (
+    <div>
+      {isLoading && <SiteLoader />}
+      <main className={`w-full mx-auto relative  ${isLoading ? 'opacity-0' : 'opacity-100'}`}>
+        <Navbar />
+        {children}
+        <Footer />
+      </main>
+    </div>
+  );
+};
---
+++ src/components/ui/other/SiteLoader.jsx
@@ -0,0 +1,44 @@
+import { motion } from 'framer-motion';
+
+const styleContainer = {
+  position: 'relative',
+  width: '150px',
+  height: '150px',
+};
+
+const styleSpan = {
+  display: 'block',
+  width: '150px',
+  height: '150px',
+  border: '4px solid #aaefaa',
+  borderTop: '4px solid #2D3134',
+  borderLeft: '4px solid #2D3134',
+  borderRadius: '50%',
+  boxSizing: 'border-box',
+  position: 'absolute',
+  top: 0,
+  left: 0,
+};
+
+const spinTransition = {
+  repeat: Infinity,
+  ease: 'easeInOut',
+  duration: 1,
+};
+
+export const SiteLoader = () => {
+  return (
+    <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-gray-950 z-50">
+      {/* Container for GIF and Spinner */}
+      <div style={styleContainer}>
+        <motion.span style={styleSpan} animate={{ rotate: 360 }} transition={spinTransition} />
+        <img
+          src="assets/loading-gif.gif"
+          alt="Loading..."
+          className="w-44 h-44" // Customize the size of the GIF
+          style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}
+        />
+      </div>
+    </div>
+  );
+};
